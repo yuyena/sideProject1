c266ac0a933a11f38686b27217f340c9cf315f2e
yuyena <yuyenabcd@gmail.com>
2025-10-01 15:22:51 +0900

    setting

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,45 +1,44 @@
 package hello.hellospring.repository;
 
-import hello.hellospring.domain.Member;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
 @Repository
-public class MemoryMemberRepository implements MemberRepository {
+public class MemoryMemberRepository  {
 
-    //long id, Member
-    private static Map<Long, Member> store = new HashMap<>();
-    private static long sequence = 0L;
-
-    @Override
-    public Member save(Member member) {
-        member.setId(++sequence);
-        store.put(member.getId(), member);
-        return member;
-    }
-
-    @Override
-    public Optional<Member> findById(Long id) {
-        //return Optional.empty();
-        return Optional.ofNullable(store.get(id));
-    }
-
-    @Override
-    public Optional<Member> findByName(String name) {
-        //멤버.getName과 parameter로 넘어온 name이 같은지 확인
-        return store.values().stream()
-                .filter(member -> member.getName().equals(name))
-                .findAny();
-
-    }
-
-    @Override
-    public List<Member> findAll() {
-        return new ArrayList<>(store.values());
-    }
-
-    public void clearStore() {
-        store.clear();
-    }
+//    //long id, Member
+//    private static Map<Long, Member> store = new HashMap<>();
+//    private static long sequence = 0L;
+//
+//    @Override
+//    public Member save(Member member) {
+//        member.setId(++sequence);
+//        store.put(member.getId(), member);
+//        return member;
+//    }
+//
+//    @Override
+//    public Optional<Member> findById(Long id) {
+//        //return Optional.empty();
+//        return Optional.ofNullable(store.get(id));
+//    }
+//
+//    @Override
+//    public Optional<Member> findByName(String name) {
+//        //멤버.getName과 parameter로 넘어온 name이 같은지 확인
+//        return store.values().stream()
+//                .filter(member -> member.getName().equals(name))
+//                .findAny();
+//
+//    }
+//
+//    @Override
+//    public List<Member> findAll() {
+//        return new ArrayList<>(store.values());
+//    }
+//
+//    public void clearStore() {
+//        store.clear();
+//    }
 
 }
